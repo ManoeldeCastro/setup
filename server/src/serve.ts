cef51ea2b539e32d5ac63d6f77b5c00deb0a4da0
manoel <manoeldiasmf@gmail.com>
2023-10-13 23:42:45 -0300

    create tables and routes

--- server/src/serve.ts
+++ server/src/serve.ts
@@ -1,19 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+
 import cors from '@fastify/cors';
 import Fastify from 'fastify';
+import { appRoutes } from './routes';
 
 
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/',async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
 
 app.listen({
   port: 3333
