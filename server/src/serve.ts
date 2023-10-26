8ffb936dd5dd2385492150a37b90bb8dcf8e8eb7
manoel <manoeldiasmf@gmail.com>
2023-10-26 19:59:47 -0300

    fix db and routes

--- server/src/serve.ts
+++ server/src/serve.ts
@@ -1,19 +1,14 @@
-
-import cors from '@fastify/cors';
-import Fastify from 'fastify';
-import { appRoutes } from './routes';
-
-
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-
 
 app.register(cors)
 app.register(appRoutes)
 
-
 app.listen({
-  port: 3333
+  port: 3333,
 }).then(() => {
-  console.log('HTTP server listening')
+  console.log('HTTP Server running!')
 })
